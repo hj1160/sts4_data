39a87f191fc0f47013ec8585c9610cf62ee4a25e
hj <gowns1939@naver.com>
2023-01-16 13:19:17 +0900

    first

---
+++ blog/src/main/java/com/cos/blog/test/TempControllerTest.java
@@ -0,0 +1,14 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TempControllerTest {
+
+	//http://localhost:8000/blog/temp/home
+	@GetMapping("/temp/home")
+	
+	
+	
+	
+	
+}
